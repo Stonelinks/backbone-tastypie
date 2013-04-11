3705401a651ede376457bcc722056c95294b0a85
Lucas Doyle <lukedoyle@mujin.co.jp>
2013-04-11 18:07:40 +0900

    format changes

--- backbone_tastypie/static/js/backbone-tastypie.js
+++ backbone_tastypie/static/js/backbone-tastypie.js
@@ -9,11 +9,12 @@
  * Modifications to work with a highly customized version of Tastypie by Lucas Doyle 2013
  *
  */
-(function(undefined ) {
+(function(undefined) {
   'use strict';
 
   // Backbone and underscore noConflict support. Save local reference to _ and Backbone objects.
   var _, Backbone;
+
   // CommonJS shim
   if (typeof window === 'undefined') {
     _ = require('underscore');
@@ -40,7 +41,7 @@
    * Modified from http://joshbohde.com/blog/backbonejs-and-django
    */
   Backbone.oldSync = Backbone.sync;
-  Backbone.sync = function(method, model, options ) {
+  Backbone.sync = function(method, model, options) {
     var headers = {};
 
     if (Backbone.Tastypie.apiKey && Backbone.Tastypie.apiKey.username) {
@@ -61,11 +62,11 @@
 
       // Set up 'success' handling
       var success = options.success;
-      dfd.done(function(resp, textStatus, xhr ) {
+      dfd.done(function(resp, textStatus, xhr) {
         _.isFunction(success) && success(resp);
       });
 
-      options.success = function(resp, textStatus, xhr ) {
+      options.success = function(resp, textStatus, xhr) {
         // If create is successful but doesn't return a response, fire an extra GET.
         // Otherwise, resolve the deferred (which triggers the original 'success' callbacks).
         if (!resp && (xhr.status === 201 || xhr.status === 202 || xhr.status === 204)) { // 201 CREATED, 202 ACCEPTED or 204 NO CONTENT; response null or empty.
@@ -84,11 +85,11 @@
 
       // Set up 'error' handling
       var error = options.error;
-      dfd.fail(function(xhr, textStatus, errorThrown ) {
+      dfd.fail(function(xhr, textStatus, errorThrown) {
         _.isFunction(error) && error(xhr.responseText);
       });
 
-      options.error = function(xhr, textStatus, errorText ) {
+      options.error = function(xhr, textStatus, errorText) {
         dfd.rejectWith(options.context || options, [xhr, textStatus, xhr.responseText]);
       };
 
@@ -125,7 +126,7 @@
   /**
    * Return the first entry in 'data.objects' if it exists and is an array, or else just plain 'data'.
    */
-  Backbone.Model.prototype.parse = function(data ) {
+  Backbone.Model.prototype.parse = function(data) {
     return data && data.objects && (_.isArray(data.objects) ? data.objects[0] : data.objects) || data;
   };
 
@@ -133,7 +134,7 @@
    * Return 'data.objects' if it exists.
    * If present, the 'data.meta' object is assigned to the 'collection.meta' var.
    */
-  Backbone.Collection.prototype.parse = function(data ) {
+  Backbone.Collection.prototype.parse = function(data) {
     if (data && data.meta) {
       this.meta = data.meta;
     }
@@ -141,7 +142,7 @@
     return data && data.objects || data;
   };
 
-  Backbone.Collection.prototype.url = function(models ) {
+  Backbone.Collection.prototype.url = function(models) {
     var url = _.isFunction(this.urlRoot) ? this.urlRoot() : this.urlRoot;
     // If the collection doesn't specify an url, try to obtain one from a model in the collection
     if (!url) {
@@ -153,7 +154,7 @@
     // Build a url to retrieve a set of models. This assume the last part of each model's idAttribute
     // (set to 'resource_uri') contains the model's id.
     if (models && models.length) {
-      var ids = _.map(models, function(model ) {
+      var ids = _.map(models, function(model) {
         var parts = _.compact(model.id.split('/'));
         return parts[parts.length - 1];
       });
@@ -163,7 +164,7 @@
     return url || null;
   };
 
-  var addSlash = function(str ) {
+  var addSlash = function(str) {
     return str + ((str.length > 0 && str.charAt(str.length - 1) === '/') ? '' : '/');
   };
 })();
